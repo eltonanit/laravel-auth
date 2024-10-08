fdd752b840eabce9d75bf43b9ee55f911c00a281
eltonanit <elton.ani@gmail.com>
2024-10-08 22:26:14 +0200

    [UPD] added dashboard

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+     @vite(['resources/js/app.js'])
+
+</head>
+<body>
+<div>
+    <div id="sidebar"></div>
+    <div id="content">
+
+    @include('partials.header')
+       
+        <main>
+            @yield('main-content')
+
+        </main>
+    </div> 
+</div>
+    
+</body>
+</html>
