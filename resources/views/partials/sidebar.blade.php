f7777288669e167acb3784457bf980f043b84658
eltonanit <elton.ani@gmail.com>
2024-10-08 23:54:57 +0200

    [UPD] added route sidebar -> Projects

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,8 +1,8 @@
  <div id="sidebar">
     <ul class="list-unstyled">
         <li class="p-2">
-            <a href="" class="text-white"> Projects </a>
-            
+            <a href="{{ route('admin.projects.index') }}" class="text-white"> Projects </a>
+
         </li>
     </ul>
  </div>
