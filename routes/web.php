895a604ec42be7ea2b89b4eefec6a8de10a7821e
eltonanit <elton.ani@gmail.com>
2024-10-08 23:43:46 +0200

    [UPD] Added route admin project controllers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProjectController as ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +26,8 @@ Route::middleware(['auth', 'verified'])
         Route::get('/', function () {
             return view('dashboard');
         })->name('dashboard');
+
+        Route::resource('/projects', ProjectController::class);
     });
 
 Route::middleware('auth')->group(function () {
