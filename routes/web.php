1fe8420f9fba0430c1df9c873e6b049d730fd057
eltonanit <elton.ani@gmail.com>
2024-10-08 22:42:28 +0200

    [UPD] added route to admin

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function() {
+        Route::get('/', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
